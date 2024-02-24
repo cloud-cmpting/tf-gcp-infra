bf620dd7b16454a71165f2bd578fd53617cf7806
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-02-23 19:18:23 -0500

    Support for creating private DB instance

--- main.tf
+++ main.tf
@@ -108,15 +108,44 @@ resource "google_compute_instance" "instance" {
 resource "google_compute_global_address" "default" {
   name = "global-psconnect-ip"
   address_type = "INTERNAL"
-  purpose = "PRIVATE_SERVICE_CONNECT"
+  purpose = "VPC_PEERING"
   network = google_compute_network.networks[var.VPCs[0].name].id
-  address = "10.3.0.5"
+  prefix_length = 16
 }
 
-resource "google_compute_global_forwarding_rule" "default" {
-  name = "globalrule"
-  target = "all-apis"
+resource "google_service_networking_connection" "private_vpc_connection" {
   network = google_compute_network.networks[var.VPCs[0].name].id
-  ip_address = google_compute_global_address.default.id
-  load_balancing_scheme = ""
+  service = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.default.name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
+}
+
+resource "google_sql_database_instance" "database" {
+  name = "private-instance-${random_id.db_name_suffix.hex}"
+  database_version = "MYSQL_5_7"
+  region = var.region
+  deletion_protection = false
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    availability_type = "REGIONAL"
+    disk_type = "pd-ssd"
+    disk_size = 100
+
+    ip_configuration {
+      ipv4_enabled = false
+      private_network = google_compute_network.networks[var.VPCs[0].name].id
+      enable_private_path_for_google_cloud_services = true
+    }
+
+    backup_configuration {
+      enabled = true
+      binary_log_enabled = true
+    }
+  }
 }
