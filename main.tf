c2d1b2fade4b2f94f7c831d9401f6e750dd1d061
Jay Vipul Gala <gala.jay1@northeastern.edu>
2024-04-03 13:17:02 -0400

    Load balancing and auto-scaling

--- main.tf
+++ main.tf
@@ -68,7 +68,7 @@ resource "google_compute_firewall" "allow" {
     ports    = [var.allowed_port]
   }
 
-  source_ranges = [var.allowed_source_range]
+  source_ranges = ["130.211.0.0/22", "35.191.0.0/16"]
   priority      = var.priority
 }
 
@@ -196,44 +196,30 @@ resource "google_project_iam_binding" "invoking" {
   ]
 }
 
-resource "google_compute_instance" "instance" {
-  name         = var.instance
+resource "google_compute_region_instance_template" "instance_template" {
+  name_prefix  = "instance-template-"
   machine_type = var.machine_type
-  zone         = var.zone
+  region       = var.region
 
-  boot_disk {
-    initialize_params {
-      image = var.boot_disk_image
-      type  = var.boot_disk_type
-      size  = var.boot_disk_size
-    }
+  disk {
+    source_image = var.boot_disk_image
+    auto_delete  = true
+    boot         = true
+    disk_type    = var.boot_disk_type
+    disk_size_gb = var.boot_disk_size
   }
 
   network_interface {
     network    = google_compute_network.networks[var.VPCs[0].name].id
     subnetwork = google_compute_subnetwork.subnets[0].id
     access_config {
-
     }
   }
 
   service_account {
-    email = google_service_account.vm_service_account.email
-    scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring.write",
-      "https://www.googleapis.com/auth/pubsub"
-    ]
+    email  = google_service_account.vm_service_account.email
+    scopes = ["https://www.googleapis.com/auth/logging.write", "https://www.googleapis.com/auth/monitoring.write", "https://www.googleapis.com/auth/pubsub"]
   }
-
-  depends_on = [
-    google_sql_database_instance.database_instance,
-    google_sql_database.database,
-    google_sql_user.user,
-    google_service_account.vm_service_account,
-    google_project_iam_binding.logging_admin_binding,
-    google_project_iam_binding.monitoring_metric_writer_binding
-  ]
 
   metadata_startup_script = <<-SCRIPT
     #!/bin/bash
@@ -251,15 +237,103 @@ resource "google_compute_instance" "instance" {
   SCRIPT
 }
 
+resource "google_compute_health_check" "health_check" {
+  name                = "health-check"
+  check_interval_sec  = 5
+  timeout_sec         = 5
+  healthy_threshold   = 2
+  unhealthy_threshold = 2
+
+  http_health_check {
+    port         = 8080
+    request_path = "/healthz"
+  }
+}
+
+resource "google_compute_region_autoscaler" "autoscaler" {
+  name   = "autoscaler"
+  region = var.region
+  target = google_compute_region_instance_group_manager.instance_group_manager.id
+
+  autoscaling_policy {
+    max_replicas    = 6
+    min_replicas    = 3
+    cooldown_period = 60
+
+    cpu_utilization {
+      target = 0.01
+    }
+  }
+
+  depends_on = [google_compute_region_instance_group_manager.instance_group_manager]
+}
+
+resource "google_compute_region_instance_group_manager" "instance_group_manager" {
+  name               = "instance-group-manager"
+  base_instance_name = "instance"
+  region             = var.region
+
+  version {
+    instance_template = google_compute_region_instance_template.instance_template.id
+  }
+
+  named_port {
+    name = "custom-port"
+    port = 8080
+  }
+
+  auto_healing_policies {
+    health_check      = google_compute_health_check.health_check.id
+    initial_delay_sec = 300
+  }
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "forwarding-rule"
+  target     = google_compute_target_https_proxy.https_proxy.id
+  port_range = "443"
+}
+
+resource "google_compute_target_https_proxy" "https_proxy" {
+  name             = "https-proxy"
+  url_map          = google_compute_url_map.url_map.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.ssl_certificate.id]
+}
+
+resource "google_compute_url_map" "url_map" {
+  name            = "url-map"
+  default_service = google_compute_backend_service.backend_service.id
+}
+
+resource "google_compute_backend_service" "backend_service" {
+  name                  = "backend-service"
+  port_name             = "custom-port"
+  protocol              = "HTTP"
+  health_checks         = [google_compute_health_check.health_check.id]
+  load_balancing_scheme = "EXTERNAL"
+
+  backend {
+    group = google_compute_region_instance_group_manager.instance_group_manager.instance_group
+  }
+}
+
+resource "google_compute_managed_ssl_certificate" "ssl_certificate" {
+  name = "ssl-certificate"
+
+  managed {
+    domains = [var.domain]
+  }
+}
+
 resource "google_dns_record_set" "a-record" {
   name         = var.domain
   type         = var.record_type
   ttl          = var.record_ttl
   managed_zone = var.dns_zone
 
-  rrdatas = [google_compute_instance.instance.network_interface[0].access_config[0].nat_ip]
+  rrdatas = [google_compute_global_forwarding_rule.forwarding_rule.ip_address]
 
-  depends_on = [google_compute_instance.instance]
+  depends_on = [google_compute_global_forwarding_rule.forwarding_rule]
 }
 
 resource "google_pubsub_topic" "topic" {
